67c5b4652e7cf70f75ce5968d97f729924f3db5a
Tolulope <lanre.toluwa@gmail.com>
2022-07-08 18:46:08 +0100

    add PaymentSuccessful handler and route: receipt page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,3 +9,30 @@ func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) 
 		app.errorLog.Println(err)
 	}
 }
+
+func (app *application) PaymentSuccessful(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	// read post data
+	cardHolderName := r.Form.Get("cardholder_name")
+	cardHolderEmail := r.Form.Get("cardholder_email")
+	paymentIntent := r.Form.Get("payment_intent")
+	paymentMethod := r.Form.Get("payment_method")
+	paymentAmount := r.Form.Get("payment_amount")
+	paymentCurrency := r.Form.Get("payment_currency")
+
+	cardHolderData := make(map[string]interface{})
+	cardHolderData["name"] = cardHolderName
+	cardHolderData["email"] = cardHolderEmail
+	cardHolderData["payment_intent"] = paymentIntent
+	cardHolderData["payment_method"] = paymentMethod
+	cardHolderData["payment_amount"] = paymentAmount
+	cardHolderData["payment_currency"] = paymentCurrency
+
+	if err := app.renderTemplate(w, r, "payment_successful", &templateData{Data: cardHolderData}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
