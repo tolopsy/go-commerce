535b6198366c337c7075c0ae6bcafb324caaa374
Tolulope <lanre.toluwa@gmail.com>
2022-07-09 12:25:16 +0100

    change pattern and handler name for payment page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
+func (app *application) PaymentTerminal(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["publishable_key"] = app.config.stripe.key
 	if err := app.renderTemplate(w, r, "terminal", &templateData{StringMap: stringMap}); err != nil {
