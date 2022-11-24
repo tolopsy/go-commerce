bb7beb217b8155dc1be20bbfbe9dbad69d0ae14a
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2022-11-24 18:17:25 +0100

    add smtp params, secret and frontend url to config

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"go-commerce/internal/driver"
@@ -25,6 +26,14 @@ type config struct {
 	db struct {
 		dsn string
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+	}
+	secretKey string
+	frontend  string
 }
 
 type application struct {
@@ -54,12 +63,18 @@ func main() {
 
 	flag.IntVar(&conf.port, "port", 9000, "Server port to listen flag on (default: 9000)")
 	flag.StringVar(&conf.env, "env", "development", "Application environment (default: development) {development|staging|production}")
+	flag.StringVar(&conf.secretKey, "secretkey", "thisisatestsecretkey", "Secret Key")
+	flag.StringVar(&conf.frontend, "frontend", "http://localhost:8000", "Frontend URL")
 
 	flag.Parse()
 
 	conf.stripe.key = os.Getenv("STRIPE_KEY")
 	conf.stripe.secret = os.Getenv("STRIPE_SECRET")
 	conf.db.dsn = os.Getenv("DB_DSN")
+	conf.smtp.host = os.Getenv("SMTP_HOST")
+	conf.smtp.port, _ = strconv.Atoi(os.Getenv("SMTP_PORT"))
+	conf.smtp.username = os.Getenv("SMTP_USERNAME")
+	conf.smtp.password = os.Getenv("SMTP_PASSWORD")
 
 	infoLog := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR:\t", log.Ldate|log.Ltime|log.Lshortfile)
@@ -75,7 +90,7 @@ func main() {
 		infoLog:  infoLog,
 		errorLog: errorLog,
 		version:  version,
-		DB: models.DBWrapper{DB: conn},
+		DB:       models.DBWrapper{DB: conn},
 	}
 
 	if err := app.serve(); err != nil {
